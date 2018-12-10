1db513dfce62d0f859e32eefe5bee486542c0bee
Dan Goodman <dan@danthegoodman.com>
2018-12-10 14:58:55 -0500

    spacing

--- leaderBoardAPI/src/client/assets/components/Welcome.js
+++ leaderBoardAPI/src/client/assets/components/Welcome.js
@@ -19,7 +19,9 @@ export default class Welcome extends React.Component {
             <div className="jumbotron">
                 <h1 className="display-2">Welcome to the Gam<span className="vim-caret">e</span></h1>
                 <br></br>
+                <br></br>
                 <h1 className="display-3" style={{textAlign: 'right'}}>UD War Games</h1>
+                <br></br>
                 <br></br>
                 <p className="lead">The goal is simple. Build up your defenses, and tear everyone else's down.</p>
                 <hr className="my-4"/>
