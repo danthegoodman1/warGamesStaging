7bafc04b478b92fd8cdc9ca746f61e6497c760a7
Dan Goodman <dan@danthegoodman.com>
2018-12-10 20:03:27 -0500

    top bar coming in

--- leaderBoardAPI/src/client/App.js
+++ leaderBoardAPI/src/client/App.js
@@ -5,6 +5,7 @@ import Table from './assets/components/Table'
 import Welcome from './assets/components/Welcome'
 import Statement from './assets/components/Statement'
 import Tabination from './assets/components/Tabination'
+import Topnav from './assets/components/Topnav'
 
 export default class App extends Component {
   state = {
@@ -18,24 +19,31 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row justify-content-center m-4">
+      <div>
+        <div className="row">
           <div className="col-md-12">
-            <Welcome />
+            <Topnav />
           </div>
         </div>
-        <div className="row justify-content-center m-4">
-          <div className="col-md-8">
-            <Statement content="Upcoming" extraMessage="Episode 1: The Wrapping of Cats"/>
+        <div className="container">
+          <div className="row justify-content-center m-4">
+            <div className="col-md-12">
+              <Welcome />
+            </div>
           </div>
-        </div>
-        <div className="row justify-content-center m-4">
-          <div className="col-md-8">
+          <div className="row justify-content-center m-4">
+            <div className="col-md-8">
+              <Statement content="Upcoming" extraMessage="Episode 1: The Wrapping of Cats"/>
+            </div>
           </div>
-        </div>
-        <div className="row justify-content-center m-4">
-          <div className="col-md-8">
-            <Tabination />
+          <div className="row justify-content-center m-4">
+            <div className="col-md-8">
+            </div>
+          </div>
+          <div className="row justify-content-center m-4">
+            <div className="col-md-8">
+              <Tabination />
+            </div>
           </div>
         </div>
       </div>
---
+++ leaderBoardAPI/src/client/assets/components/Topnav.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+/** 
+ * @param content (String) message
+ * @param caret (Boolean) whether the last character should have the vim caret
+ * @param extraMessage (String) extra message to say
+ */
+export default class Topnav extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+
+        if (true) {
+            const sendIt = (
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                        data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <a className="navbar-brand" href="#!">Navbar1</a>
+
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul className="navbar-nav mr-auto mt-2 mt-md-0">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#!">Home
+                                <span className="sr-only">(current)</span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#!">Link</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#!">Disabled</a>
+                            </li>
+                        </ul>
+                        <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+                        </form>
+                    </div>
+                </nav>
+            )
+            return sendIt
+        } else {
+            const sendIt = (
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                        data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <a className="navbar-brand" href="#!">Navbar2</a>
+
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul className="navbar-nav mr-auto mt-2 mt-md-0">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#!">Home
+                                <span className="sr-only">(current)</span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#!">Link</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#!">Disabled</a>
+                            </li>
+                        </ul>
+                        <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+                        </form>
+                    </div>
+                </nav>
+            )
+            return (sendIt)
+        }
+
+    }
+}
