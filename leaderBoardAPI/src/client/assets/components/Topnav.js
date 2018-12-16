136608d746629730aff7b9630a92edbc2e3a47c3
Dan Goodman <dan@danthegoodman.com>
2018-12-16 09:22:41 -0500

    comment

--- leaderBoardAPI/src/client/assets/components/Topnav.js
+++ leaderBoardAPI/src/client/assets/components/Topnav.js
@@ -146,12 +146,12 @@ const Topnav = observer(class Topnav extends React.Component {
                             <span className="sr-only">(current)</span>
                             </a>
                         </li>
-                        <li className="nav-item">
+                        {/* <li className="nav-item">
                             <a className="nav-link" href="#!">Link</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link disabled" href="#!">Disabled</a>
-                        </li>
+                        </li> */}
                     </ul>
                     { this.state.setProfile && this.state.profile && <a onClick={this.signOut.bind(this)} className="btn btn-primary">
                         Sign out, {this.state.profile.nickname} <FontAwesomeIcon icon="user" />
